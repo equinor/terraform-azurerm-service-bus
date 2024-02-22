a00ccd821b6c484f7fe044cb91d57aa090155331
Kristian Vallejos <126077239+kov117@users.noreply.github.com>
2024-02-22 09:17:05 +0100

    feat: variable for diagnostic settng metric (#38)

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+  diagnostic_setting_metric_categories = ["AllMetrics"]
+}
+
 resource "azurerm_servicebus_namespace" "this" {
   name                = var.namespace_name
   location            = var.location
@@ -23,8 +27,13 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
     }
   }
 
-  metric {
-    category = "AllMetrics"
-    enabled  = true
+  dynamic "metric" {
+    for_each = toset(concat(local.diagnostic_setting_metric_categories, var.diagnostic_setting_enabled_metric_categories))
+
+    content {
+      # Azure expects explicit configuration of both enabled and disabled metric categories.
+      category = metric.value
+      enabled  = contains(var.diagnostic_setting_enabled_metric_categories, metric.value)
+    }
   }
 }

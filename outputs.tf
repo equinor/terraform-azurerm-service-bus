17c04c4fa03cab998bee1b684544ac3202a0464a
Morten Aursland <90191126+BouvetMorten@users.noreply.github.com>
2024-03-21 07:54:43 +0100

    feat: add managed identity (#43)
    * implement identity
    
    * add identity outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "namespace_endpoint" {
   description = "The URL to access the ServiceBus Namespace."
   value       = azurerm_servicebus_namespace.this.endpoint
 }
+
+output "identity_principal_id" {
+  description = "The Principal ID for the Service Principal associated with the Managed Service Identity of this ServiceBus Namespace. This value will be null if no identity is assigned."
+  value       = try(azurerm_servicebus_namespace.this.identity[0].principal_id, null)
+}
+
+output "identity_tenant_id" {
+  description = "The Tenant ID for the Service Principal associated with the Managed Service Identity of this ServiceBus Namespace. This value will be null if no identity is assigned."
+  value       = try(azurerm_servicebus_namespace.this.identity[0].tenant_id, null)
+}

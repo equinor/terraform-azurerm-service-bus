b9bda61d44c4ea05b700d5f803c6a1e0771ba4ff
FrankN <fnaerla@outlook.com>
2024-09-20 13:46:56 +0200

    added queue and auth resources

--- main.tf
+++ main.tf
@@ -53,6 +53,36 @@ resource "azurerm_servicebus_namespace" "this" {
   tags = var.tags
 }
 
+resource "azurerm_servicebus_namespace_authorization_rule" "this" {
+  for_each = var.namespace_authorization_rule
+
+  name         = each.value.name
+  namespace_id = azurerm_servicebus_namespace.this.id
+  listen       = each.value.listen
+  send         = each.value.send
+  manage       = each.value.manage
+}
+
+resource "azurerm_servicebus_queue" "this" {
+  for_each = var.queue
+
+  name                 = each.value.name
+  namespace_id         = azurerm_servicebus_namespace.this.id
+  partitioning_enabled = each.value.partitioning_enabled
+}
+
+resource "azurerm_servicebus_queue_authorization_rule" "this" {
+  depends_on = [azurerm_servicebus_queue.this]
+  # depends_on = ["${azurerm_servicebus_queue.this}[${each.value.queue_name}]"]
+  for_each = var.queue_authorization_rule
+
+  name     = each.value.name
+  queue_id = "${azurerm_servicebus_namespace.this.id}/queues/${each.value.queue_name}"
+  listen   = each.value.listen
+  send     = each.value.send
+  manage   = each.value.manage
+}
+
 resource "azurerm_monitor_diagnostic_setting" "this" {
   name                       = var.diagnostic_setting_name
   target_resource_id         = azurerm_servicebus_namespace.this.id

8b8859715c6f44078cf9d459ccc29b2920204d7f
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-09-23 08:43:45 +0200

    chore: add networking unit tests

---
+++ tests/setup/outputs.tf
@@ -0,0 +1,23 @@
+output "namespace_name" {
+  value = "sbns-${local.name_suffix}"
+}
+
+output "resource_group_name" {
+  value = local.resource_group_name
+}
+
+output "location" {
+  value = "northeurope"
+}
+
+output "log_analytics_workspace_id" {
+  value = "/subscriptions/${local.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.OperationalInsights/workspaces/log-${local.name_suffix}"
+}
+
+output "subnet_ids" {
+  value = [
+    "/subscriptions/${local.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.Network/virtualNetworks/vnet-${local.name_suffix}/subnets/snet-${local.name_suffix}-01",
+    "/subscriptions/${local.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.Network/virtualNetworks/vnet-${local.name_suffix}/subnets/snet-${local.name_suffix}-02",
+    "/subscriptions/${local.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.Network/virtualNetworks/vnet-${local.name_suffix}/subnets/snet-${local.name_suffix}-03"
+  ]
+}

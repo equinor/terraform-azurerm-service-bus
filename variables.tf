5c93795851f4010485a590d971e6b59ff5798535
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-09-30 13:19:04 +0200

    chore: update variable and output descriptions (#60)
    * wip: update variable and output descriptions
    
    * chore: update variable descriptions
    
    * chore: update output descriptions

--- variables.tf
+++ variables.tf
@@ -1,60 +1,78 @@
 variable "namespace_name" {
-  description = "The name of the Servicebus Namespace"
+  description = "The name of this Service Bus namespace."
   type        = string
+  nullable    = false
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group to create the resources in."
   type        = string
+  nullable    = false
 }
 
 variable "location" {
   description = "The location to create the resources in."
   type        = string
+  nullable    = false
 }
 
 variable "sku" {
-  description = "Defines which SKU to use. Options are Basic, Standard or Premium."
+  description = "The SKU to use for this Service Bus namespace. Value must be \"Basic\", \"Standard\" or \"Premium\"."
   type        = string
   default     = "Basic"
+  nullable    = false
 }
 
 variable "local_auth_enabled" {
   description = "Should local authentication using shared access signatures (SAS) be enabled for this Service Bus namespace? If value is false, Microsoft Entra authentication must be used instead."
   type        = bool
   default     = false
+  nullable    = false
 }
 
 variable "capacity" {
-  description = "Specifies the capacity. Premium allows 1, 2, 4, 8 or 16. Basic or Standard allows 0 only."
+  description = "Specifies the capacity for this Service Bus namespace. Value must be 1, 2, 4, 8 or 16. Only applicable if value of sku is \"Premium\"."
   type        = number
   default     = 1
   nullable    = false
+
+  validation {
+    condition     = contains([1, 2, 4, 8, 16], var.capacity)
+    error_message = "Capacity must be 1, 2, 4, 8 or 16."
+  }
 }
 
 variable "premium_messaging_partitions" {
-  description = "Only valid when sku is Premium and the minimum number is 1. Possible values include 0, 1, 2, and 4. Defaults to 0"
+  description = "Specifies the number of messaging partitions for this Service Bus namespace. Value must be 1, 2 or 4. Only applicable if value of sku is \"Premium\"."
   type        = number
   default     = 1
   nullable    = false
+
+  validation {
+    condition     = contains([1, 2, 4], var.premium_messaging_partitions)
+    error_message = "Premium messaging partitions must be 1, 2 or 4."
+  }
 }
 
 variable "public_network_access_enabled" {
-  description = "Is public network access enabled for the Service Bus Namespace?"
+  description = "Is public network access enabled for the Service Bus namespace? Only applicable if value of sku is \"Premium\"."
   type        = bool
   default     = true
+  nullable    = false
 }
 
 variable "system_assigned_identity_enabled" {
   description = "Should the system-assigned identity be enabled for this Service Bus namespace?"
   type        = bool
   default     = false
+  nullable    = false
 }
 
 variable "identity_ids" {
   description = "A list of IDs of managed identities to be assigned to this Service Bus namespace."
   type        = list(string)
   default     = []
+  nullable    = false
 }
 
 variable "network_rule_set_default_action" {
@@ -69,31 +87,35 @@ variable "network_rule_set_default_action" {
 }
 
 variable "network_rule_set_ip_rules" {
-  description = "One or more IP Addresses, or CIDR Blocks which should be able to access the ServiceBus Namespace."
+  description = "A list of public IP addresses or ranges that should be able to bypass the network rule set for this Service Bus namespace. Values must be in CIDR format. Only applicable if value of sku is \"Premium\"."
   type        = list(string)
   default     = []
+  nullable    = false
 }
 
 variable "network_rule_set_virtual_network_rules" {
-  description = "Conditionally define multiple network_rules inside the network_rule_set"
+  description = "A list of virtual network subnets that should be able to bypass the network rule set for this Service Bus namespace. Only applicable if value of sku is \"Premium\"."
 
   type = list(object({
     subnet_id                            = string
     ignore_missing_vnet_service_endpoint = optional(bool, false)
   }))
 
-  default = []
+  default  = []
+  nullable = false
 }
 
 variable "network_rule_set_trusted_services_allowed" {
-  description = "Azure Services that are known and trusted for this resource type are allowed to bypass firewall configuration"
+  description = "Should Azure services be allowed to bypass the network rule set for this Service Bus namespace? Only applicable if value of sku is \"Premium\"."
   type        = bool
   default     = true
+  nullable    = false
 }
 
 variable "log_analytics_workspace_id" {
   description = "The ID of the Log Analytics workspace to send diagnostics to."
   type        = string
+  nullable    = false
 }
 
 variable "diagnostic_setting_enabled_log_categories" {
@@ -106,16 +128,19 @@ variable "diagnostic_setting_enabled_metric_categories" {
   description = "A list of metric categories to be enabled for this diagnostic setting."
   type        = list(string)
   default     = []
+  nullable    = false
 }
 
 variable "diagnostic_setting_name" {
-  description = "The name of this Diagnostic Setting."
+  description = "The name of this diagnostic setting."
   type        = string
   default     = "audit-logs"
+  nullable    = false
 }
 
 variable "tags" {
   description = "A map of tags to assign to the resources."
   type        = map(string)
   default     = {}
+  nullable    = false
 }

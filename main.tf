7cd1d45db2b26aa8d5362514912e54328c2d7a45
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-10-01 11:39:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,9 +23,10 @@ resource "azurerm_servicebus_namespace" "this" {
     for_each = var.sku == "Premium" ? [0] : []
 
     content {
+      public_network_access_enabled = var.public_network_access_enabled
+
       # The 'default_action' can only be set to "Allow" if no 'ip_rules' or 'network_rules' is set.
       default_action                = length(var.network_rule_set_ip_rules) == 0 && length(var.network_rule_set_virtual_network_rules) == 0 ? "Allow" : "Deny"
-      public_network_access_enabled = var.public_network_access_enabled
       ip_rules                      = var.network_rule_set_ip_rules
       trusted_services_allowed      = var.network_rule_set_trusted_services_allowed
 
